c0d0edc855c45700790e32fae402693560fbf1ca
Fudy-V <fudyV.test@gmail.com>
2024-11-11 01:20:11 +0900

    ✨ inputページの実装

---
+++ src/app/input/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Header from "@/components/Header/Header";
+import React from "react";
+
+export default function input() {
+  return (
+    <>
+      <Header title="Register TODO" transitionUrl="./" buttonText="完了" />
+    </>
+  );
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 type Title = {
   title: string;
+  buttonText: string;
+  transitionUrl: string;
 };
 
 export default function Header(props: Title) {
@@ -10,7 +13,9 @@ export default function Header(props: Title) {
     <>
       <header className="flex flex-row justify-around py-2 border-b-2 border-gray-500">
         <h1 className="text-2xl font-bold">{props.title}</h1>
-        <Button>登録</Button>
+        <Button>
+          <Link href={props.transitionUrl}>{props.buttonText}</Link>
+        </Button>
       </header>
     </>
   );
