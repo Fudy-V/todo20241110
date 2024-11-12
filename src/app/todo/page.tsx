81ac739472088cb5742f0f3a8dc7fd25698bd5d5
Fudy-V <fudyV.test@gmail.com>
2024-11-12 23:46:58 +0900

    ✨ Listのui構築（仮）

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header/Header";
+import { TodoList } from "@/components/List/TodoList";
 
 export default function Home() {
   return (
     <>
       <Header title="TODO LIST" buttonText="登録" />
       <main className="px-4 flex flex-row justify-center">
-        <div className="w-10 h-10 bg-black"></div>
+        {/* <div className="w-10 h-10 bg-black"></div> */}
+        <TodoList />
       </main>
     </>
   );
